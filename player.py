c7209363d628ddedc8ed6835fffabfb89b229db5
aWEY <coolawey@gmail.com>
2015-01-31 17:06:50 +0800

    show window (x,y)

--- player.py
+++ player.py
@@ -44,6 +44,7 @@ def play(window, fname, adj):
 			#time.sleep()
 	except curses.error:
 		window.addstr(0,0,"Windows size is too small.X(")
+        window.addstr(0,0,"Current Windows Size X, Y: %s, %s"% window.getmaxyx())
 		window.addstr(1,0,"Expand window size and try again.")
 		window.addstr(2,0,"Press any key to exit...")
 		window.refresh()
